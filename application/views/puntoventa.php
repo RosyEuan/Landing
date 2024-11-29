77bc8b2c6fa420ce889b83a1eb760433ab4b1105
Angelou5 <tunjafet97@gmail.com>
2024-11-29 13:16:42 -0500

    Views/Modificacion de landing

--- application/views/puntoventa.php
+++ application/views/puntoventa.php
@@ -634,7 +634,7 @@ function logout() {
       <div class="modal-dialog modal-lg">
         <div class="modal-content">
           <div class="modal-header">
-            <h5 class="modal-title" id="pagoModalLabel">Formulario de Pago</h5>
+            <h5 class="modal-title" id="pagoModalLabel">Formulario de Pagos</h5>
             <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
           </div>
           <div class="modal-body">
